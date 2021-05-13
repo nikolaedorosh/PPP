58390c5d75f87820b410193c221a7addae7e6a72
Nick <belov_nikolai@mail.ru>
2021-05-13 16:19:56 +0300

    reset to 0.1

--- front-end/src/redux/initState.js
+++ front-end/src/redux/initState.js
@@ -1,6 +1,1 @@
-const initState = {
-  isSignedIn: false,
-  userID: null,
-};
 
-export default initState;
--- front-end/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import initState from "./initState";
-import rootReducer from "./reducers/allReducers";
-
-const store = createStore(rootReducer, initState );
-
-export default store;
