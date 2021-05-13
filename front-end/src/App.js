73aed681d1451044cd2881814ac2e82258ab9f12
Nico <nikolae.dorosh@gmail.com>
2021-05-13 10:19:59 +0300

    rel Navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import PageNotFound from "./components/404/404";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/signup'>
+            <p>signup</p>
+          </Route>
+          <Route exact path='/signin'>
+            <p>signin</p>
+          </Route>
+          <Route exact path='/'>
+            <p>profile</p>
+          </Route>
+          <Route exact path='/logger'>
+            <p>logger</p>
+          </Route>
+          <Route>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
