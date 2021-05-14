58ed89873a5dd2d34c88b6f9c6686dc66beca9f4
Sergio <bernaldojunior@gmail.com>
2021-05-14 12:07:38 +0300

    graf-comp-v2

--- back-end/app.js
+++ back-end/app.js
@@ -4,37 +4,33 @@ const MongoStore = require("connect-mongo");
 const logger = require("morgan");
 const path = require("path");
 const { connect } = require("mongoose");
+const cors = require('cors')
+const connectDB = require('./db/connect')
 require("dotenv").config();
 const User = require("./models/userModel");
 
 const indexRouter = require("./routes/indexRouter");
+const loggerRouter = require("./routes/loggerRouter")
+
 
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.static(path.join(process.env.PWD, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
+
 
 app.use("/", indexRouter);
+app.use("/logger" , loggerRouter)
+
 
 const PORT = process.env.PORT || 2208;
 
+// const SERVER_PORT = 8080
 app.listen(PORT, () => {
-  console.log(`SERVER ON ${PORT}.`);
-
-  connect(
-    /*process.env.DB_CONNECTION,*/ "mongodb://localhost:27017/Elbrus-Family",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    },
-    () => {
-      console.log("DATABASE ON 27017");
-    }
-  );
-});
+  console.log(`Server has been started on port ${PORT}...`);
+  connectDB()
+})
 
 module.exports = app;
--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -1,9 +1,42 @@
-const { Schema, model } = require("mongoose");
+const {Schema, model} = require('mongoose')
 
 const userSchema = new Schema({
-  name: {
-    type: String,
-  },
-});
+  // info : [
+  //   {
+  //     age:Number,
+  //     gender:String,
+  //     weight: Number,
+  //     height:Number,
+  //     activity:Boolean,
+  //   },
+  // ],
+  // food : [
+  //   {
+  //     date:Number,
+  //     kcal:Number,
+  //     Proteins:Number,
+  //     carbohydrates: Number,
+  //     fats:Number
+  //   }
+  // ],
+  // target : [
+  //   {
+  //   weigthKG:Number,
+  //   kcalNow: Number,
+  //   ProteinsNow:Number,
+  //   carbohydratesNow: Number,
+  //   fatsNow:Number
+  //   }
+  // ]
+  
+      weigthKG:Number,
+      kcalNow: Number,
+      ProteinsNow:Number,
+      carbohydratesNow: Number,
+      fatsNow:Number,
+  
+})
 
-module.exports = model("User", userSchema);
+const userModel = model("User", userSchema);
+
+module.exports = userModel;
