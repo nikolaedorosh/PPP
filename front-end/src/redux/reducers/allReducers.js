47b19afb49b26ff52699ba3f4f9554362bb2f1cc
Sergio <bernaldojunior@gmail.com>
2021-05-14 14:09:07 +0300

    add all modals

--- front-end/src/redux/reducers/allReducers.js
+++ front-end/src/redux/reducers/allReducers.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import foodReducer from "./foodReducer";
 import authReducer from "./userReducer";
-
+import graphicsReducers from './graphicsReducers'
 export default combineReducers({
   auth: authReducer,
-  meal: foodReducer
+  meal: foodReducer,
+  graphics: graphicsReducers,
 });
