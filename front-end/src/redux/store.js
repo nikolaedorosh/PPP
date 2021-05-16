6c267ecc9e4ffc1bff98bf334e4e0ca7c9b8f880
Margo <margosha.novikova@gmail.com>
2021-05-16 13:40:30 +0300

    something went wrong with the previous one

--- front-end/src/redux/store.js
+++ front-end/src/redux/store.js
@@ -1,7 +1,6 @@
 import rootReducer from "./reducers/rootReduces";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
-
 import initState from "./initState"
 import { composeWithDevTools } from 'redux-devtools-extension';
 import watchAddLoad from './saga';
@@ -16,15 +15,5 @@ const sagaMiddleware = createSagaMiddleware();
   );
 
   sagaMiddleware.run(watchAddLoad);
-
-export default store;
-
-
-
-const store = createStore(
-  rootReducer,
-  initState,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
